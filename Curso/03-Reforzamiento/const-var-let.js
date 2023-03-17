4660dfc7dd46cf16d3f3d29ac7a424238f9873c1
Enrique Alberto Navia <anavia_n@outlook.com>
2023-03-16 21:05:42 -0600

    Finish let vs var

---
+++ Curso/03-Reforzamiento/const-var-let.js
@@ -0,0 +1,22 @@
+//Todo depende del ambito 
+let nombre = 'Wolverine'
+console.log(nombre)
+
+if (true) {
+    //Dentro de este scope si nos regresa magneto
+    let nombre = 'Magneto'
+    console.log(nombre)
+}
+
+
+
+
+//Todo depende del ambito 
+const nombre01 = 'Wolverine'
+console.log(nombre)
+
+if (true) {
+    //No va a cambiar aunque este dentro de un scope diferente
+    const nombre01 = 'Magneto'
+    console.log(nombre)
+}
